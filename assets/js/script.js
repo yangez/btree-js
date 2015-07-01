796b17a5e058ecacf81bd2572d3df5a7de3d632d
Eric Yang <e@eric-y.com>
2015-07-01 18:53:59 -0500

    Added assets file structure and added d3.js library for visualization

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,9 +19,51 @@ for(var i=0; i<count; i++) {
   bTree.insert(current, true);
 }
 
+
+
+
+console.log(generateData());
+
 printKeys();
 
-// displays keys
+
+function generateData() {
+
+  /*
+  var treeData = [
+  {
+    "name": "Top Level",
+    "parent": "null",
+    "children": [
+      {
+        "name": "Level 2: A",
+        "parent": "Top Level",
+        "children": [
+          {
+            "name": "Son of A",
+            "parent": "Level 2: A"
+          },
+          {
+            "name": "Daughter of A",
+            "parent": "Level 2: A"
+          }
+        ]
+      },
+      {
+        "name": "Level 2: B",
+        "parent": "Top Level"
+      }
+    ]
+  }
+];
+*/
+}
+
+function dataTraverse(node) {
+  
+}
+
+// displays keys in console
 function printKeys() {
   root = bTree.root
 
