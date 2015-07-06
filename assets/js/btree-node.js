434028f7687114229836d56d26bcfea368217b86
Allen Chang <voxlol@outlook.com>
2015-07-05 21:31:16 -0500

    Removed edge case of huge

--- assets/js/btree-node.js
+++ assets/js/btree-node.js
@@ -29,8 +29,9 @@ BTreeNode.prototype.traverse = function(value, strict) {
 }
 
 BTreeNode.prototype.insert = function(value){
+
   var int = parseInt(value) || 0;
-  if (int <= 0) {
+  if ( int <= 0 || int > 1000000000000 ) {
     alert('Please enter a valid integer.');
     return false;
   }
