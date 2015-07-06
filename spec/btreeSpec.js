3c9ae53b24c03e7fef3c3bf27049074ce8a1f59e
voxlol <voxlol@outlook.com>
2015-07-06 03:21:18 -0500

    Added testing basic code for the 5 rules that satisfy a B-Tree of order m as by Knuth's definition of a B-Tree

--- spec/btreeSpec.js
+++ spec/btreeSpec.js
@@ -1,3 +1,36 @@
+describe("BTree Definition", function() {
+  it('every node has at most m children', function() {
+    // test order 3-10
+
+    // check the children length of each node
+  });
+
+  it('every non-leaf node (except root) has at least m/2 children', function(){
+    // test order 3-10
+
+    // check that each node has minimum children requirements met
+  });
+
+  it('the root has at least 2 children if it is not a leaf node', function(){
+    // test order 3-30
+
+    // Root must have 2 children if it's not a leaf. if the breadth is > 1 level then must have >=2 children
+  });
+
+  jt('a non-leaf node with k children contains k-1 keys', function(){
+    // test order 3-10
+
+    // key-to-children relationship must be maintained
+  });
+
+  jt('all leaves appear in the same level', function(){
+    // test order 3-10
+
+    // use isLeaf() to test that all on same breadth
+  });
+
+});
+
 describe("BTree", function() {
 
   // beforeEach(function() {
@@ -38,7 +71,3 @@ describe("BTree", function() {
   });
 });
 
-describe("BTree Node", function() {
-  describe('Insert', function() {
-  });
-});
